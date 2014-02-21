197880173c059e90c7175937dcd5eafe22f71fdc
Bastien Faure <b0z@hackerzvoice.net>
2014-02-21 12:18:58 +0100

    Added loadrc function (thx to vbernat)

--- rc.lua
+++ rc.lua
@@ -11,6 +11,40 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local menubar = require("menubar")
 
+-- {{{ Simple function to load additional LUA files from rc/.
+function loadrc(name, mod)
+   local success
+   local result
+
+   -- Which file? In rc/ or in lib/?
+   local path = awful.util.getdir("config") .. "/" ..
+      (mod and "lib" or "rc") ..
+      "/" .. name .. ".lua"
+
+   -- If the module is already loaded, don't load it again
+   if mod and package.loaded[mod] then return package.loaded[mod] end
+
+   -- Execute the RC/module file
+   success, result = pcall(function() return dofile(path) end)
+   if not success then
+      naughty.notify({ title = "Error while loading an RC file",
+               text = "When loading `" .. name ..
+              "`, got the following error:\n" .. result,
+               preset = naughty.config.presets.critical
+             })
+      return print("E: error loading RC file '" .. name .. "': " .. result)
+   end
+
+   -- Is it a module?
+   if mod then
+      return package.loaded[mod]
+   end
+
+   return result
+end
+-- }}}
+
+
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
