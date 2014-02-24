7d812e3012a43f2bbee5b772c1c7a3cedd6a37e3
Bastien Faure <b0z@hackerzvoice.net>
2014-02-25 00:24:19 +0100

    Added keybinding : filebrowser launcher

--- rc/bindings.lua
+++ rc/bindings.lua
@@ -70,7 +70,9 @@ globalkeys = awful.util.table.join(
     -- Personal keybindings
     awful.key({ "Control" }, "k", function () awful.util.spawn("keepassx") end),
     awful.key({ "Control" }, "l", function () awful.util.spawn("xtrlock") end),
-    awful.key({ modkey    }, "b", function () awful.util.spawn("chromium-browser") end)
+    awful.key({ modkey    }, "b", function () awful.util.spawn("chromium-browser") end),
+    awful.key({ modkey    }, "f", function () awful.util.spawn("nautilus") end)
+    
 )
 
 clientkeys = awful.util.table.join(
