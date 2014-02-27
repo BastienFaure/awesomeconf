eeb2e83061b213660a3c0c087feb3c9d2f4d07ba
Bastien Faure <b0z@hackerzvoice.net>
2014-02-27 14:28:33 +0100

    Restructuration for associating layout to tags

--- rc/tags.lua
+++ rc/tags.lua
@@ -1,8 +1,12 @@
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
-tags = {}
+tags = {
+    names = { 1, 2, 3, 4, 5, 6, 7, "mail", "social" ,"skype", "jabber" },
+    layout = { layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2] }
+}
+
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, "mail", "social"}, s, layouts[1])
+    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, "mail", "social", "skype", "jabber" }, s, tags.layout)
 end
 -- }}}
