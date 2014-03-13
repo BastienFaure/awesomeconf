f4d1a34edff32f3534a505db6aa1b3f098f9e5c9
Bastien Faure <b0z@hackerzvoice.net>
2014-03-13 11:58:56 +0100

    Defaut layout to tiled

--- rc/tags.lua
+++ rc/tags.lua
@@ -2,7 +2,7 @@
 -- Define a tag table which hold all screen tags.
 tags = {
     names = { 1, 2, 3, 4, 5, 6, 7, "mail", "social" ,"skype", "jabber" },
-    layout = { layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2] }
+    layout = { layouts[2], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2] }
 }
 
 for s = 1, screen.count() do
