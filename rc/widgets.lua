4572e7f3d153375c7a0c06e0e7f6c421fd116db2
Bastien Faure <bastien@faure.io>
2014-11-07 01:30:16 +0100

    disable volume widget and add volumeicon at start

--- rc/widgets.lua
+++ rc/widgets.lua
@@ -8,35 +8,37 @@ netwidget = wibox.widget.textbox()
 -- separator
 separator = wibox.widget.textbox()
 separator:set_text(" :: ")
+
 -- volume widget
-volume_widget = wibox.widget.textbox()
-volume_widget:set_align("right")
+--volume_widget = wibox.widget.textbox()
+--volume_widget:set_align("right")
+--
+--function update_volume(widget)
+--   local fd = io.popen("amixer sget Master")
+--   local status = fd:read("*all")
+--   fd:close()
+--
+--   -- local volume = tonumber(string.match(status, "(%d?%d?%d)%%")) / 100
+--   local volume = string.match(status, "(%d?%d?%d)%%")
+--   volume = string.format("% 3d", volume)
+--
+--   status = string.match(status, "%[(o[^%]]*)%]")
+--
+--   if string.find(status, "on", 1, true) then
+--       -- For the volume numbers
+--       volume = volume .. "%"
+--   else
+--       -- For the mute button
+--       volume = volume .. "M"
+--
+--   end
+--   widget:set_markup(volume)
+--end
+--
+--mytimer = timer({ timeout = 1 })
+--mytimer:connect_signal("timeout", function () update_volume(volume_widget) end)
+--mytimer:start()
 
-function update_volume(widget)
-   local fd = io.popen("amixer sget Master")
-   local status = fd:read("*all")
-   fd:close()
-
-   -- local volume = tonumber(string.match(status, "(%d?%d?%d)%%")) / 100
-   local volume = string.match(status, "(%d?%d?%d)%%")
-   volume = string.format("% 3d", volume)
-
-   status = string.match(status, "%[(o[^%]]*)%]")
-
-   if string.find(status, "on", 1, true) then
-       -- For the volume numbers
-       volume = volume .. "%"
-   else
-       -- For the mute button
-       volume = volume .. "M"
-
-   end
-   widget:set_markup(volume)
-end
-
-mytimer = timer({ timeout = 1 })
-mytimer:connect_signal("timeout", function () update_volume(volume_widget) end)
-mytimer:start()
 -- clock
 mytextclock = awful.widget.textclock()
 -- memory
@@ -73,7 +75,7 @@ cpuwidget:set_color({
 })
 
 -- Register widget
-vicious.register(netwidget, vicious.widgets.net, '<span color="#CC9393">${p3p1 down_kb}</span> <span color="#7F9F7F">${p3p1 up_kb}</span>', 3)
+vicious.register(netwidget, vicious.widgets.net, '<span color="#CC9393">${enp8s0 down_kb}</span> <span color="#7F9F7F">${enp8s0 up_kb}</span>', 3)
 update_volume(volume_widget)
 vicious.register(memwidget, vicious.widgets.mem, "$1", 1)
 vicious.register(cpuwidget, vicious.widgets.cpu, "$1", 1)
