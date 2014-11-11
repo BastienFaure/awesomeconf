52f97a12128dd27a1750cd8d9cfc7a7cdde9005c
Bastien Faure <bastien@faure.io>
2014-11-11 01:44:02 +0100

    extra function for further use

--- rc.lua
+++ rc.lua
@@ -12,6 +12,19 @@ beautiful = require("beautiful")
 naughty = require("naughty")
 menubar = require("menubar")
 
+-- {{{ Function used for capturing the output of os.execute
+function os.capture(cmd, raw)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  if raw then return s end
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  return s
+end
+-- }}}
+
 -- {{{ Simple function to load additional LUA files from rc/.
 function loadrc(name, mod)
    local success
